c4b7f40737b066e44754c0c289e436f2e7b5830c
Dean Fox <deanfoxcd@gmail.com>
2024-10-05 21:55:53 -0400

    183-188

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,80 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Selecting, creating, and deleting
+/*
+// Selecting
+const header = document.querySelector('.header');
+// This will be a nodelist
+const allSections = document.querySelector('.section');
+
+//This will be an html collection. It will be constantly updated if elemenst are added or removed programatically. So if a function removes a button for example then this list will reflect that. A nodelist won't update in the same way
+const allButtons = document.getElementsByTagName('button');
+*/
+
+// Creating and inserting
+/*
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies'
+message.innerHTML =
+  'We use cookies. <button class="btn btn--close-cookie">Got it<button>';
+// header.prepend(message);
+header.append(message); // Overwrites the prepend (moves the element)
+// header.append(message.cloneNode(true)); // To copy an element to 2 places.
+
+// header.before(message)
+// header.after(message)
+*/
+
+// Delete
+/*
+// New way
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.remove());
+
+// Old way
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', () => message.parentElement.removeChild());
+*/
+
+// Styles
+/*
+message.style.backgroundColor = '#37383d';
+message.style.width = '100%';
+console.log(getComputedStyle(message).color);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// Pulls out the number from the height, adds 30, turns it back to a tring and adds px
+
+// For custom properties
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+*/
+
+// Attributes
+/*
+const logo = document.querySelector('.nav__logo');
+console.log(logo.src);
+// Can only read standard attributes of the element, not custom ones
+console.log(logo.designer); // Won't work
+console.log(logo.getAttribute('designer')); // Will work
+
+logo.alt = 'Bieautiful minimalist logo';
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.getAttribute('src'));
+// Shows relative file path. Will be different from logo.src
+
+// Data attributes
+// Special attributes that have to start with data-
+console.log(logo.dataset.versionNumber);
+
+// Classes
+//add, remove, toggle, contains
+*/
